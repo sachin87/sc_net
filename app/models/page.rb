62433eec919722d5ebd9ca4b48438baef14295d7
sachin87 <singhsachin87@yahoo.com>
2011-12-18 21:12:11 +0530

    initial setup and page model and controller added

---
+++ app/models/page.rb
@@ -0,0 +1,14 @@
+class Page < ActiveRecord::Base
+  validates :title, :body , :presence => true
+  validates :title, :length => [3..255]
+  validates :body, :length => { :maximum => 10000 }
+
+  def before_create
+    @attributes['permalink'] = title.downcase.gsub(/\s+/, '_').gsub(/[^a-zA-Z0-9_]+/, '')
+  end
+
+  def to_param
+    "#{id}-#{permalink}"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetworking::Application.config.secret_token = 'e07439c5994d2347f9b2bbfecc851b5243647fba37a72b299174976bf5f957b2969a4e080ec04994673a2c87651a2f32bf5a0fce7a22e9c57c43ce9bae913d48'
