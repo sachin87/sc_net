8fdb21c38cef654983741060dcafeea3852e534a
Sachin87 <singhsachin87@yahoo.com>
2012-03-11 18:21:08 +0530

    test commit

--- db/migrate/20120129100822_add_roles_to_user.rb
+++ db/migrate/20120129100822_add_roles_to_user.rb
@@ -3,6 +3,7 @@ class AddRolesToUser < ActiveRecord::Migration
     User.create(:email => 'sachin.y87@gmail.com', :password => '12345678', :password_confirmation => '12345678', :enabled => true)
     ['Administrator', 'Moderator', "Editor"].each do |name|
       Role.create(:name => name)
+
     end
     admin_user = User.first
     admin_user.roles << Role.all
