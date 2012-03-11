e05e06b629467bca8bdc53562d7bf120587315af
Sachin87 <singhsachin87@yahoo.com>
2012-03-11 18:13:41 +0530

    added user to migration

--- db/migrate/20120129100822_add_roles_to_user.rb
+++ db/migrate/20120129100822_add_roles_to_user.rb
@@ -1,5 +1,6 @@
 class AddRolesToUser < ActiveRecord::Migration
   def change
+    User.create(:email => 'sachin.y87@gmail.com', :password => '12345678', :password_confirmation => '12345678', :enabled => true)
     ['Administrator', 'Moderator', "Editor"].each do |name|
       Role.create(:name => name)
     end
