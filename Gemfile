ffcda231dc443a3f5139a4f0afb561a3c22524a6
Sachin87 <singhsachin87@yahoo.com>
2012-03-11 18:38:00 +0530

    bug fixed

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'pony'
 gem 'RedCloth'
 gem 'cancan'
 gem 'heroku'
+gem 'foreman'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  hhas_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_presence :photo
   belongs_to :user
 end
