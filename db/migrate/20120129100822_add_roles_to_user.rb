9a8711f6898dd7248bbf70ce4b926cb5528c8159
Sachin Singh <sachin.y87@gmail.com>
2012-05-11 23:03:59 +0530

    migration bug fixed

--- db/migrate/20120129100822_add_roles_to_user.rb
+++ db/migrate/20120129100822_add_roles_to_user.rb
@@ -1,11 +1,10 @@
 class AddRolesToUser < ActiveRecord::Migration
   def change
-    User.create(:email => 'sachin.y87@gmail.com', :password => '12345678', :password_confirmation => '12345678', :enabled => true)
-    ['Administrator', 'Moderator', "Editor"].each do |name|
-      Role.create(:name => name)
-
-    end
-    admin_user = User.first
-    admin_user.roles << Role.all
+    #User.create(:email => 'sachin.y87@gmail.com', :password => '12345678', :password_confirmation => '12345678', :enabled => true)
+    #['Administrator', 'Moderator', "Editor"].each do |name|
+     # Role.create(:name => name)
+    #end
+    #admin_user = User.first
+    #admin_user.roles << Role.all
   end
 end
